2a1b8e685f88b7434457f6e1faf02800be2ba9fe
Agustin <francoagustinfacundo@gmail.com>
2019-07-07 13:41:35 -0300

    cambios en detalle

---
+++ js/detalle.js
@@ -0,0 +1,16 @@
+function mostrarModal() {
+    $("#successModal").modal({
+        fadeDuration: 1000,
+        fadeDelay: 0.50,
+        escapeClose: false,
+        clickClose: false        
+    });
+}
+
+
+function initializeDetalle() {
+}
+
+$(document).ready(function () {
+    initializeDetalle();
+});
